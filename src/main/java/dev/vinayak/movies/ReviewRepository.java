6fbd75725057ad14f6f1a8e90c07ad7b8809a639
vinayak roy <44561838+royvinayak@users.noreply.github.com>
2025-08-13 01:14:16 +0530

    Initial commit

---
+++ src/main/java/dev/vinayak/movies/ReviewRepository.java
@@ -0,0 +1,11 @@
+package dev.vinayak.movies;
+
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface ReviewRepository extends MongoRepository<Review, String> {
+
+}
---
+++ src/main/java/dev/vinayak/movies/WebConfig.java
@@ -0,0 +1,18 @@
+package dev.vinayak.movies;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // allow React frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true); // allow cookies or headers
+    }
+}
